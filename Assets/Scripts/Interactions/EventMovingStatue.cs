d3c233fa8ff4fc23942539f901bbf40453fd0521
minjeparkk <zone8572@gmail.com>
2025-08-18 10:26:55 +0900

    Feat
    움직이는 석상 이벤트 추가

---
+++ Assets/Scripts/Interactions/EventMovingStatue.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventMovingStatue : MonoBehaviour
+{
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponentInParent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // 특정 태그를 가진 객체가 충돌했을 때만 작동하도록 설정
+        if (other.CompareTag("Player"))
+        {
+            // 애니메이션 파라미터 없이 애니메이션 클립 이름으로 바로 재생
+            animator.Play("StatueMove");
+        }
+    }
+}
