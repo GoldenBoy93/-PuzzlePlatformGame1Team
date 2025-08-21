4fda2a577842f2d537c0dd85464c53fb83df97e2
minjeparkk <zone8572@gmail.com>
2025-08-21 20:50:34 +0900

    Feat
    경비연출 구현중

---
+++ Assets/Scripts/Interactions/EventSecurity.cs
@@ -0,0 +1,52 @@
+using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.InputSystem.XR;
+
+public class EventSecurity : MonoBehaviour
+{
+    private Animator animator;
+    private AudioSource audioSource;
+    GameObject securityGuard;
+    private bool hasTriggered = false;
+    public CinemachineFreeLook _cinematicCam;
+
+
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        securityGuard = Resources.Load<GameObject>("SecurityGuard");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && !hasTriggered)
+        {
+            hasTriggered = true;
+
+            Debug.Log("Security call");
+            Instantiate(securityGuard, new Vector3(7, -7, -19), Quaternion.Euler(0, 0, 0));
+
+            
+        }
+    }
+
+    public IEnumerator Sec()
+    {
+        if (_cinematicCam != null)
+        {
+            Player.Instance._controller.LockOnInput(1);
+            Cursor.lockState = CursorLockMode.Locked;
+            // 연출 시간 대기
+            _cinematicCam.Priority = 0;
+            yield return new WaitForSeconds(4f);
+            // 연출 끝나면 입력 활성화
+            _cinematicCam.Priority = 10;
+            Cursor.lockState = CursorLockMode.None;
+            Player.Instance._controller.LockOnInput(0);
+        }
+    }
+}
